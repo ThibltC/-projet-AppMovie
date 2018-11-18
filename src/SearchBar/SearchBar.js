4026afdd6861e826b08de1a1dd52f9c53cde5450
ThibltC <yojibo@hotmail.fr>
2018-11-18 17:53:38 +0100

    test heroku 3

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -8,7 +8,7 @@ class SearchBar extends Component {
             <form autoComplete='off' className='SearchBar'>
                 <input
                     type='text'
-                    placeholder={`Entrer un filmeuuuuuh`}
+                    placeholder={`Entrer le nom d'un film`}
                     value={this.props.inputSearch}
                     onChange={e => this.props.changeInput(e)}
                 />
