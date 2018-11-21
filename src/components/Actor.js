18a210737a902df03c32bc0920937de86dc6124b
ThibltC <yojibo@hotmail.fr>
2018-11-21 14:19:51 +0100

    pasredux finalement

--- src/components/Actor.js
+++ src/components/Actor.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import ListMovies from '../ListMovies/ListMovies';
+import ListMovies from '../components/ListMovies';
 
 import './Actor.css';
 
