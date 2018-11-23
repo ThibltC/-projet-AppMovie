7c70d85eafe5dc62aeb4429780013c23e59c3c64
ThibltC <yojibo@hotmail.fr>
2018-11-23 18:06:50 +0100

    on y est presque

--- src/components/Search.js
+++ src/components/Search.js
@@ -90,37 +90,46 @@ class Search extends Component {
     render() {
         const { yearMax, yearMin, namesGenreSelected, runTimeMax } = this.state
         return (
-            <div className='Search' >
-                <h2>Choisissez vos critères</h2>
-                <Link to='/'>
-                    <button className='backHome'>Retour à l'accueil</button>
-                </Link>
-                <div className='listGenres'>
-                    {this.state.genres.map(e => {
-                        return (
-                            <div className='genre' key={e.name} onClick={event => this.addOrRemoveGenre(event, e.id, e.name)}>
-                                {e.name}
-                            </div>
-                        )
-                    })}
-                </div>
-                <form>
-                    <input type="range" name="runtime" min="0" max="240" step='1' value={runTimeMax} onChange={e => this.changeRunTimeMax(e)} />{this.convertMinToHours(runTimeMax)}
-                    <input type="range" name="yearMin" min="1907" max={yearMax} step='1' value={yearMin} onChange={e => this.changeYearMin(e)} />{yearMin}
-                    <input type="range" name="yearMax" min={yearMin} max="2019" step='1' value={yearMax} onChange={e => this.changeYearMax(e)} />{yearMax}
-                </form>
-                <p>{`Films compris entre ${yearMin} et ${yearMax} dont la durée ne dépasse pas ${this.convertMinToHours(runTimeMax)}`}</p>
-                {namesGenreSelected.length ?
-                    <p>Vous avez choisi : {`${namesGenreSelected}`}</p>
-                    :
-                    <p>Pas de genre selectionné</p>
-                }
-                <div className='displayButtons'>
-                    <button onClick={this.seachMovies}>Lancer la rechercher</button>
-                    <button onClick={e => window.location.reload()}>Effacer tout</button>
-                </div>
+            <div className='SearchComponent'>
+                <div className='Search' >
+                    <h2>Choisissez vos critères</h2>
+                    <div className='displayButtons'>
+                        <Link to='/'>
+                            <button className='backHome'>Retour à l'accueil</button>
+                        </Link>
+                    </div>
+                    <div className='paramsDisplay'>
+                        <div className='listGenres'>
+                            {this.state.genres.map(e => {
+                                return (
+                                    <div className='genre' key={e.name} onClick={event => this.addOrRemoveGenre(event, e.id, e.name)}>
+                                        {e.name}
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <form>
+                            <input type="range" name="runtime" min="0" max="240" step='1' value={runTimeMax} onChange={e => this.changeRunTimeMax(e)} />
+                            Durée Max : {this.convertMinToHours(runTimeMax)}
+                            <input type="range" name="yearMin" min="1907" max={yearMax} step='1' value={yearMin} onChange={e => this.changeYearMin(e)} />
+                            Année Min : {yearMin}
+                            <input type="range" name="yearMax" min={yearMin} max="2019" step='1' value={yearMax} onChange={e => this.changeYearMax(e)} />
+                            Année Max : {yearMax}
+                        </form>
+                    </div>
 
-                <div className="response">
+                    <p>{`Films compris entre ${yearMin} et ${yearMax} dont la durée ne dépasse pas ${this.convertMinToHours(runTimeMax)}`}</p>
+                    {namesGenreSelected.length ?
+                        <p>Vous avez choisi : {`${namesGenreSelected}`}</p>
+                        :
+                        <p>Pas de genre selectionné</p>
+                    }
+                    <div className='displayButtons'>
+                        <a href='#response'><button onClick={this.seachMovies}>Lancer la rechercher</button></a>
+                        <button onClick={e => window.location.reload()}>Effacer tout</button>
+                    </div>
+                </div>
+                <div id='response' className="response">
                     {this.state.moviesFound
                         .filter((_, i) => i < 15)
                         .map((element, i) =>
