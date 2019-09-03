290cb9a89fc0edf6e2213264b4e3d52fbee3f998
ThibltC <yojibo@hotmail.fr>
2019-09-03 21:07:06 +0200

    [bug] comment randomMovie in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,17 +25,17 @@ class Header extends Component {
             <header className="Header">
                 <img src={logo} className="Main-logo" alt="logo" onClick={this.changeRandomMovie}/>
                 <h1>Donuts Movies</h1>
-                {this.props.moviesIsLoaded &&
+                {/* {this.props.moviesIsLoaded &&
                     <Link to={`/movie${randomMovie.id}`} >
                         <div className='randomHeaderStyle'>
-                            <div className='filtreImage'></div>
+                            <div className='filtreImage'/>
                             {randomMovie.backdrop_path ?
                              <img src={`https://image.tmdb.org/t/p/original${randomMovie.backdrop_path}`} alt={randomMovie.title} />
                             :
                             <img src={`https://image.tmdb.org/t/p/original${randomMovie.poster_path}`} alt={randomMovie.title} />}            
                         </div>
                     </Link>
-                }
+                } */}
             </header>
         )
     }
